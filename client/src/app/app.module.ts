30b962658b48bbfdc1231d0cbacf1dafbeb242f4
Jefferson Neves <jefferson.rpn@gmail.com>
2019-10-11 19:07:03 -0300

    Cria componentes para criação de visualizações

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { GraphChartComponent } from './graph-chart/graph-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GraphChartComponent
   ],
   imports: [
     BrowserModule,
